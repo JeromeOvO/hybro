5e5f50a995c14bbb3625f04645a66755ac7881b8
Kevin(Kefa) Lu <kefa.lu@gmail.com>
2026-06-28 16:56:20 -0700

    chore: remove proprietary route registrations from api_gateway router

--- backend/api_gateway/router.py
+++ backend/api_gateway/router.py
@@ -6,14 +6,11 @@ from api_gateway.routes import (
     a2a_task_routes,
     agent_group_routes,
     agent_routes,
-    discovery_api_key_routes,
-    discovery_routes,
     files_routes,
     hitl_routes,
     hub_routes,
     inspection_routes,
     memory_routes,
-    platform_gateway_routes,
     relay_routes,
     room_routes,
     sse_routes,
@@ -42,16 +39,7 @@ def build_api_gateway_router() -> APIRouter:
     gateway_router.include_router(sse_routes.router, tags=["sse"])
     gateway_router.include_router(agent_group_routes.router, tags=["agent_group"])
     gateway_router.include_router(files_routes.router, tags=["files"])
-    gateway_router.include_router(discovery_routes.router, tags=["discovery"])
-    gateway_router.include_router(
-        discovery_api_key_routes.router,
-        tags=["api_keys"],
-    )
     gateway_router.include_router(a2a_task_routes.router, tags=["a2a_tasks"])
-    gateway_router.include_router(
-        platform_gateway_routes.router,
-        tags=["gateway"],
-    )
     gateway_router.include_router(relay_routes.router, tags=["relay"])
     gateway_router.include_router(webhook_routes.router, tags=["webhooks"])
 
